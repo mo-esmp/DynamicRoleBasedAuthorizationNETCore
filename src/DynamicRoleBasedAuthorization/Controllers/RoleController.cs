278776cf241d5d4cefe361ee3474f29304ad2d7a
Mohsen Esmailpour <mo.esmp@hotmail.com>
2018-06-01 18:57:24 +0430

    Role controller added.

---
+++ src/DynamicRoleBasedAuthorization/Controllers/RoleController.cs
@@ -0,0 +1,100 @@
+﻿using DynamicRoleBasedAuthorization.Services;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DynamicRoleBasedAuthorization.Controllers
+{
+    public class RoleController : Controller
+    {
+        private readonly IMvcControllerDiscovery _mvcControllerDiscovery;
+
+        public RoleController(IMvcControllerDiscovery mvcControllerDiscovery)
+        {
+            _mvcControllerDiscovery = mvcControllerDiscovery;
+        }
+
+        // GET: Role
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: Role/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: Role/Create
+        public ActionResult Create()
+        {
+            ViewData["Controllers"] = _mvcControllerDiscovery.GetControllers();
+
+            return View();
+        }
+
+        // POST: Role/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(IFormCollection collection)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Role/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: Role/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, IFormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Role/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: Role/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
