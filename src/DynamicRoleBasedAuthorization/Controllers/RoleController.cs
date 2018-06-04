168fc4f33b62e8aaff3fd831fa8a73dc513f9fb6
Mohsen Esmailpour <mo.esmp@hotmail.com>
2018-06-04 14:21:53 +0430

    Create role done.

--- src/DynamicRoleBasedAuthorization/Controllers/RoleController.cs
+++ src/DynamicRoleBasedAuthorization/Controllers/RoleController.cs
@@ -1,16 +1,23 @@
-﻿using DynamicRoleBasedAuthorization.Services;
+﻿using DynamicRoleBasedAuthorization.Models;
+using DynamicRoleBasedAuthorization.Services;
 using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using System.Linq;
+using System.Threading.Tasks;
 
 namespace DynamicRoleBasedAuthorization.Controllers
 {
     public class RoleController : Controller
     {
         private readonly IMvcControllerDiscovery _mvcControllerDiscovery;
+        private readonly RoleManager<ApplicationRole> _roleManager;
 
-        public RoleController(IMvcControllerDiscovery mvcControllerDiscovery)
+        public RoleController(IMvcControllerDiscovery mvcControllerDiscovery, RoleManager<ApplicationRole> roleManager)
         {
             _mvcControllerDiscovery = mvcControllerDiscovery;
+            _roleManager = roleManager;
         }
 
         // GET: Role
@@ -37,18 +44,36 @@ namespace DynamicRoleBasedAuthorization.Controllers
         // POST: Role/Create
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Create(IFormCollection collection)
+        public async Task<ActionResult> Create(RoleViewModel viewModel)
         {
-            try
-            {
-                // TODO: Add insert logic here
+            ViewData["Controllers"] = _mvcControllerDiscovery.GetControllers();
 
-                return RedirectToAction(nameof(Index));
-            }
-            catch
+            if (!ModelState.IsValid)
+                return View(viewModel);
+
+            var role = new ApplicationRole { Name = viewModel.Name };
+            if (viewModel.SelectedControllers != null && viewModel.SelectedControllers.Any())
             {
-                return View();
+                foreach (var controller in viewModel.SelectedControllers)
+                {
+                    foreach (var action in controller.Actions)
+                    {
+                        action.ControllerId = controller.Id;
+                    }
+                }
+
+                var accessJson = JsonConvert.SerializeObject(viewModel.SelectedControllers);
+                role.Access = accessJson;
             }
+
+            var result = await _roleManager.CreateAsync(role);
+            if (result.Succeeded)
+                return RedirectToAction(nameof(Index));
+
+            foreach (var error in result.Errors)
+                ModelState.AddModelError("", error.Description);
+
+            return View(viewModel);
         }
 
         // GET: Role/Edit/5
