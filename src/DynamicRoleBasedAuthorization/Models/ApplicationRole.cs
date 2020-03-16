ebc892656dcf08f42af7719a6a8046960f5a2de5
Mohsen Esmailpour <mo.esmp@hotmail.com>
2020-03-16 13:31:18 +0330

    Remove old sample.

--- src/DynamicRoleBasedAuthorization/Models/ApplicationRole.cs
+++
@@ -1,9 +0,0 @@
-﻿using Microsoft.AspNetCore.Identity;
-
-namespace DynamicRoleBasedAuthorization.Models
-{
-    public class ApplicationRole : IdentityRole
-    {
-        public string Access { get; set; }
-    }
-}
