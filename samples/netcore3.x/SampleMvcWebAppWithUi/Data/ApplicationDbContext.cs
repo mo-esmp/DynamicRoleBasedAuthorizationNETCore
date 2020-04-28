b6062e9a7091fc6475928eea011c3ff69d073bd1
Mohsen Esmailpour <mo.esmp@hotmail.com>
2020-04-28 23:12:29 +0430

    Add another sample application.

---
+++ samples/netcore3.x/SampleMvcWebAppWithUi/Data/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using SampleMvcWebAppWithUi.Models;
+
+namespace SampleMvcWebAppWithUi.Data
+{
+    public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+    }
+}
