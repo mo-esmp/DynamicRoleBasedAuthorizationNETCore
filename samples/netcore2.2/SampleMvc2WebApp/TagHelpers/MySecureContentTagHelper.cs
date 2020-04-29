54195b0f420ff62a7fd8da70d6ea5afc02393faf
Mohsen Esmailpour <mo.esmp@hotmail.com>
2020-04-29 20:41:36 +0430

    Add secure tag helper to samples.

---
+++ samples/netcore2.2/SampleMvc2WebApp/TagHelpers/MySecureContentTagHelper.cs
@@ -0,0 +1,20 @@
+﻿using DynamicAuthorization.Mvc.Core;
+using DynamicAuthorization.Mvc.Core.Models;
+using Microsoft.AspNetCore.Razor.TagHelpers;
+using SampleMvc2WebApp.Data;
+
+namespace SampleMvc2WebApp
+{
+    [HtmlTargetElement("secure-content")]
+    public class MySecureContentTagHelper : SecureContentTagHelper<ApplicationDbContext>
+    {
+        public MySecureContentTagHelper(
+            ApplicationDbContext dbContext,
+            DynamicAuthorizationOptions authorizationOptions,
+            IRoleAccessStore roleAccessStore
+            )
+            : base(dbContext, authorizationOptions, roleAccessStore)
+        {
+        }
+    }
+}
