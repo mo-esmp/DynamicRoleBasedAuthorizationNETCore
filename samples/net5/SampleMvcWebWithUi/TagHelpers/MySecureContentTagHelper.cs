98d9e0d1ea8a97aa4ad8caf8cb269fbac5d5a150
Mohsen Esmailpour <mo.esmp@gmail.com>
2020-11-14 16:02:50 +0330

    Add sample for .NET 5.

---
+++ samples/net5/SampleMvcWebWithUi/TagHelpers/MySecureContentTagHelper.cs
@@ -0,0 +1,20 @@
+﻿using DynamicAuthorization.Mvc.Core;
+using DynamicAuthorization.Mvc.Core.Models;
+using Microsoft.AspNetCore.Razor.TagHelpers;
+using SampleMvcWebWithUi.Data;
+
+namespace SampleMvcWebWithUi
+{
+    [HtmlTargetElement("secure-content")]
+    public class MySecureContentTagHelper : SecureContentTagHelper<ApplicationDbContext>
+    {
+        public MySecureContentTagHelper(
+            ApplicationDbContext dbContext,
+            DynamicAuthorizationOptions authorizationOptions,
+            IRoleAccessStore roleAccessStore
+            )
+            : base(dbContext, authorizationOptions, roleAccessStore)
+        {
+        }
+    }
+}
