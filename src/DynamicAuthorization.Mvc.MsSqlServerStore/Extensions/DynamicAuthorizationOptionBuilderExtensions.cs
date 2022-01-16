aa5b009ec1eeb945a307e8a1ed195423d4030b02
Mohsen Esmailpour <mo.esmp@gmail.com>
2022-01-17 00:54:55 +0400

    Add SQL Server data store option builder.

---
+++ src/DynamicAuthorization.Mvc.MsSqlServerStore/Extensions/DynamicAuthorizationOptionBuilderExtensions.cs
@@ -0,0 +1,49 @@
+﻿using DynamicAuthorization.Mvc.Core;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using System;
+
+namespace DynamicAuthorization.Mvc.MsSqlServerStore.Extensions
+{
+    /// <summary>
+    ///   SQL Server data store specific extension methods for <see cref="IDynamicAuthorizationOptionBuilder"/>.
+    /// </summary>
+    public static class DynamicAuthorizationOptionBuilderExtensions
+    {
+        /// <summary>
+        ///   Configures the Dynamic Authorization to connect to a SQL Server database.
+        /// </summary>
+        /// <param name="optionsBuilder">
+        ///   The optionsBuilder being used to configure the Dynamic Authorization.
+        /// </param>
+        /// <param name="options">An action to allow configure SQL Server data store.</param>
+        /// <exception cref="ArgumentNullException">throw if optionsBuilder is null.</exception>
+        /// <exception cref="ArgumentNullException">throw is options is null.</exception>
+        public static IDynamicAuthorizationOptionBuilder UseSqlServer(this IDynamicAuthorizationOptionBuilder optionsBuilder, Action<SqlOptions> options)
+        {
+            if (optionsBuilder == null)
+                throw new ArgumentNullException(nameof(optionsBuilder));
+
+            if (options == null)
+                throw new ArgumentNullException(nameof(options));
+
+            var serviceProvider = optionsBuilder.Services.BuildServiceProvider();
+            var scope = serviceProvider.CreateScope();
+            var logger = scope.ServiceProvider.GetService<ILogger<SqlTableCreator>>();
+
+            var sqlOptions = new SqlOptions();
+            options.Invoke(sqlOptions);
+
+            var tableCreator = new SqlTableCreator(sqlOptions, logger);
+            tableCreator.CreateTable();
+
+            scope.Dispose();
+            serviceProvider.Dispose();
+
+            optionsBuilder.Services.AddSingleton(sqlOptions);
+            optionsBuilder.Services.AddScoped<IRoleAccessStore, RoleAccessStore>();
+
+            return optionsBuilder;
+        }
+    }
+}
