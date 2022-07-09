83b6e0b5d189de77ac76457f1b9a16eee886fc0e
mesmailpour-spectra <mohsen@projectspectra.dev>
2022-07-09 13:25:14 +0200

    Adjust DynamicAuthorizationOptionBuilder extensions for SQL Server data store.

--- src/DynamicAuthorization.Mvc.MsSqlServerStore/Extensions/DynamicAuthorizationOptionBuilderExtensions.cs
+++ src/DynamicAuthorization.Mvc.MsSqlServerStore/Extensions/DynamicAuthorizationOptionBuilderExtensions.cs
@@ -13,21 +13,22 @@ namespace DynamicAuthorization.Mvc.MsSqlServerStore.Extensions
         /// <summary>
         ///   Configures the Dynamic Authorization to connect to a SQL Server database.
         /// </summary>
-        /// <param name="optionsBuilder">
-        ///   The optionsBuilder being used to configure the Dynamic Authorization.
+        /// <param name="builder">
+        ///   The options builder being used to configure the Dynamic Authorization.
         /// </param>
         /// <param name="options">An action to allow configure SQL Server data store.</param>
-        /// <exception cref="ArgumentNullException">throw if optionsBuilder is null.</exception>
+        /// <exception cref="ArgumentNullException">throw if options builder is null.</exception>
         /// <exception cref="ArgumentNullException">throw is options is null.</exception>
-        public static IDynamicAuthorizationOptionBuilder UseSqlServer(this IDynamicAuthorizationOptionBuilder optionsBuilder, Action<SqlOptions> options)
+        /// <returns>The same authorization option builder so that multiple calls can be chained.</returns>
+        public static IDynamicAuthorizationOptionBuilder UseSqlServerStore(this IDynamicAuthorizationOptionBuilder builder, Action<SqlOptions> options)
         {
-            if (optionsBuilder == null)
-                throw new ArgumentNullException(nameof(optionsBuilder));
+            if (builder == null)
+                throw new ArgumentNullException(nameof(builder));
 
             if (options == null)
                 throw new ArgumentNullException(nameof(options));
 
-            var serviceProvider = optionsBuilder.Services.BuildServiceProvider();
+            var serviceProvider = builder.Services.BuildServiceProvider();
             var scope = serviceProvider.CreateScope();
             var logger = scope.ServiceProvider.GetService<ILogger<SqlTableCreator>>();
 
@@ -40,10 +41,10 @@ namespace DynamicAuthorization.Mvc.MsSqlServerStore.Extensions
             scope.Dispose();
             serviceProvider.Dispose();
 
-            optionsBuilder.Services.AddSingleton(sqlOptions);
-            optionsBuilder.Services.AddScoped<IRoleAccessStore, RoleAccessStore>();
+            builder.Services.AddSingleton(sqlOptions);
+            builder.Services.AddScoped<IRoleAccessStore, RoleAccessStore>();
 
-            return optionsBuilder;
+            return builder;
         }
     }
 }
